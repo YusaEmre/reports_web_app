8bb1b80b2dac4d1138b1a0b80271275a4ddf3fb4
yusaincedere <yusaincedere@gmail.com>
2022-10-14 11:02:38 +0300

    sad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div> Ass </div>
+        <HomePage/>
       </header>
     </div>
   );
