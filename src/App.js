8f090111ee529b351e207b73e824a66bb7c861eb
yusaincedere <yusaincedere@gmail.com>
2022-10-14 11:15:27 +0300

    sdas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
 import HomePage from './pages/HomePage';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Switch>
+      <Route exact path="/">
+        <HomePage/>
+      </Route>
+    </Switch>
+      </div>;
 }
 
 export default App;
